24c84cd9c8a98733cdc42e7644be285b2321dfa9
Douglas Urner <dlu@canishe.com>
2018-01-17 16:17:00 -0800

    Start PlayerMovement.cs script.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float xSpeed = 1f;
+	public float ySpeed = 1f;
+	public float hopPower = 20f;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		Vector3 impulse = new Vector3(
+			xSpeed * Input.GetAxis("Horizontal"),
+			ySpeed * Input.GetAxis("Vertical"),
+			0 // Keyboard only gives us two axes.
+			);
+		
+		// Use space bar to hop.
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			impulse.y += hopPower;
+		}
+		
+		rb.AddForce(impulse);
+	}
+}
